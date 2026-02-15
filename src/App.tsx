65ba8117971dc68c6bd59e2bb49e824b3244765c
Rika Maeda <scaricky3132@gmail.com>
2026-02-15 19:15:14 +0900

    Issue1 型定義と初期データ作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import type { Todo } from './types/todos.ts'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // Todoの配列をstateで持つ
+  // 型は Todo[]（Todoの配列）
+  const [todos, setTodos] = useState<Todo[]>// ← TypeScriptの説明。このstateにはTodoの配列が入る
+  ([
+    {id:1,
+      title:"React Todoを作る",
+      status: "notStarted",
+      detail: "まず土台"
+    },
+    {id:2,
+      title:"一覧UIを作る",
+      status: "inProgress",
+      detail: "mapで表示",
+    },
+    {id:3,
+      title:"Issueで管理する",
+      status: "done",
+      detail: "進捗を見える化"
+    }
+  ])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>React Todo</h1>
     </>
   )
 }
---
+++ src/types/todos.ts
@@ -0,0 +1,24 @@
+// /Users/admin/react-todo/src/types/todos.js
+
+// Todoの進捗状態を表す型
+// 3つの文字列のどれかしか入らないように制限している
+// → 間違った文字を入れるとTypeScriptがエラーにしてくれる
+export type TodoStatus = "notStarted" | "inProgress" | "done";
+
+
+// Todo1件のデータの形（設計図）を定義している
+// → Todoは必ずこの形で作るというルール
+export type Todo = {
+
+  // Todoを識別するための番号
+  // 一位の値を入れる
+  id: number;
+  // Todoのタイトル
+  title: string;
+  // Todoの進捗状態
+  // 上で定義したTodoStatus型しか入らない
+  status: TodoStatus;
+  // 詳細説明
+  // メモや補足を書く欄
+  detail: string;
+};
